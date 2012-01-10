3e6bcaff00005b85a4005e8eb4bef1f7234260c7
matt <elhotiby104@yahoo.com>
2012-01-10 16:00:14 -0500

    changes

---
+++ public/javascripts/custom.js
@@ -0,0 +1,11 @@
+jQuery(function ($) {
+	
+		//     $('#my_projects').click(function(e){
+		// 	if($(this).hasClass('expanded')){
+		// 		$(this).removeClass('expanded');
+		// 	}else{
+		// 		$(this).addClass('expanded');
+		// 	}
+		// });
+	
+});
