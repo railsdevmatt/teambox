6d8e6571fed6330d0a039f231232c9edcd1cbdf6
matt <elhotiby104@yahoo.com>
2012-01-11 09:59:46 -0500

    .gitignore is now working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,8 +7,8 @@ Teambox::Application.configure do
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
-
+  config.action_controller.perform_caching = false
+  config.action_view.debug_rjs = true
   # RAILS3 investigate
   # config.action_view.cache_template_loading            = true
 
--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,11 +1,14 @@
 jQuery(function ($) {
 	
-		//     $('#my_projects').click(function(e){
-		// 	if($(this).hasClass('expanded')){
-		// 		$(this).removeClass('expanded');
-		// 	}else{
-		// 		$(this).addClass('expanded');
-		// 	}
-		// });
+		    $('.nav_links .el').click(function(e) {
+					e.preventDefault();
+					$(this).next(".contained").slideToggle();
+				});
+				
+				$('.new_project, .time_tracking, .el a').click(function() {
+					if(!$(this).hasClass('no_go')){
+						window.location = $(this).attr('href');
+					}
+				});
 	
 });
